f3fccc4f44ba5f4dd54800433c1995322ed66f39
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-11 23:55:03 +0100

    obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, seprator: str) -> str:
-    ''' Returns messages obsuscated.
-    '''
+    ''' Returns messages obsuscated. '''
     for field in fields:
         message = re.sub(f'{field}=.+?{seprator}',
                          f'{field}={redaction}{seprator}', message)

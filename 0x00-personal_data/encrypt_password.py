e7b085166596bb0d135bd6afca94f3f8c8231500
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-12 01:13:54 +0100

    salt & hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 ''' Password Encryption Module
 '''
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     ''' Returns a hashed password '''
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' Validates that the provided password matches the hashed password '''
+    return checkpw(password.encode('utf-8'), hashed_password)

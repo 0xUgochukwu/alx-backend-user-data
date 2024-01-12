cb4f4a0f996af96dcb2e8d2820903fdb46d13c8b
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-12 01:25:34 +0100

    validate

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+''' Password Encryption Module
+'''
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
+    ''' Returns a hashed password '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
-    """
+    ''' Validates that the provided password matches the hashed password '''
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

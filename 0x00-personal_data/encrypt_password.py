6e36ddf995dcde1b1190012416c4294cafdebddb
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-12 01:22:09 +0100

    fix: encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-''' Password Encryption Module
-'''
-from bcrypt import hashpw, gensalt, checkpw
+""" Returns a salted, hashed password, byte in string """
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    ''' Returns a hashed password '''
-    return hashpw(password.encode('utf-8'), gensalt())
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    ''' Validates that the provided password matches the hashed password '''
-    return checkpw(password.encode('utf-8'), hashed_password)
+    """ Implement is_valid to validate provided password
+    matched hashed_password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

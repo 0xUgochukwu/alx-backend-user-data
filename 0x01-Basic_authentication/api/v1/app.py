100d638a30dbc2e244678190e9cb4ef4890553e3
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-16 12:05:07 +0100

    unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,11 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """ Not Authorized
+    """
+    return jsonify({"error": "Unathorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")

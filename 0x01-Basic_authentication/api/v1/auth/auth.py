6a33e7f35920cfbfc52aa14e15ed56c9f76b9a3a
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-16 12:35:43 +0100

    Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" User Authentication Module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication Class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Requires Auth
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Auth Header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current User
+        """
+        return None

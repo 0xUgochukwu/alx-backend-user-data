9d3beef624c7a1ab816ecf3036df908c43f389b8
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-16 13:33:38 +0100

    basic_auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic User Authentication Module
+"""
+from api.v1.auth.auth import Auth
+from flask import request
+from typing import List, TypeVar
+
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication Class
+    """
+    pass

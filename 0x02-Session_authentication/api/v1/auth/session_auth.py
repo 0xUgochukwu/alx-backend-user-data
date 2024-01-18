9eea790a39e75d80a0279e96100169b4115b3580
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-19 00:05:02 +0100

    session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Session Authentication Module
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Session Authentication Class
+    """
+    pass

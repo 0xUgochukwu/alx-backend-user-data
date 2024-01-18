d2aabed8c9e2b7e6b32bb62f55ac11e45095d65b
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-19 00:16:57 +0100

    session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        if user_id and type(user_id) is None:
+        if user_id and type(user_id) is str:
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id

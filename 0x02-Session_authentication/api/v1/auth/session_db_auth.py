8e0490a9f8c3407c2d98e21dc28d8cc15b8fad25
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-19 02:05:46 +0100

    db session

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,42 +2,42 @@
 """ Session Authentication Module (Database)
 """
 from api.v1.auth.session_auth import SessionAuth
-from datetime import datetime, timedelta
-from os import getenv
+from models.user_session import UserSession
 
 
-class SessionExpAuth(SessionAuth):
-    """ Session Expiration Authentication Class
+class SessionDBAuth(SessionAuth):
+    """ Session Database Authentication Class
     """
-    def __init__(self):
-        """ Constructor
-        """
-        self.session_duration = int(getenv('SESSION_DURATION', 0))
-
     def create_session(self, user_id: str = None) -> str:
         """ Creates a session
         """
         session_id = super().create_session(user_id)
         if session_id:
-            self.user_id_by_session_id[session_id] = {
-                'user_id': user_id,
-                'created_at': datetime.now()
-            }
+            session = UserSession(user_id=user_id, session_id=session_id)
+            session.save()
+            UserSession.save_to_file()
             return session_id
         return None
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Returns user id based on a session id
         """
-        if session_id and type(session_id) is str:
-            session_dict = self.user_id_by_session_id.get(session_id)
-            if session_dict:
-                if self.session_duration <= 0:
-                    return session_dict.get('user_id')
-                if 'created_at' in session_dict:
-                    if (session_dict['created_at'] +
-                            timedelta(seconds=self.session_duration) <
-                            datetime.now()):
-                        return None
-                return session_dict.get('user_id')
-            return None
+        if session_id:
+            UserSession.load_from_file()
+            session = UserSession.search({'session_id': session_id})
+            if session:
+                return session[0].user_id
+        return None
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout
+        """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                session = UserSession.search({'session_id': session_id})
+                if session:
+                    session[0].remove()
+                    UserSession.save_to_file()
+                    return True
+        return False

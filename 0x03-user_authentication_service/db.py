cc86b53c8bd4e9edae1c845364dda5dc8c800ce2
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-23 14:23:58 +0100

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,7 +54,7 @@ class DB:
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
-        if User is None:
+        if user is None:
             raise NoResultFound
 
         return user

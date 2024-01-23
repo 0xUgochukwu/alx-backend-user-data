938a760d67badc4c958c3038bfd787c728cf0b9f
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-23 14:05:42 +0100

    nullable

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,8 @@ class User(Base):
     """ The User Model
     """
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

79ebac09c61e97498e1b7fbb2f63fa28628fd627
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-23 16:04:16 +0100

    get user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,6 +75,20 @@ def log_out() -> str:
         abort(403)
 
 
+@app.route("/profile", methods=["GET"])
+def profile() -> str:
+    """ GET /profile
+        Return:
+          - JSON payload
+    """
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": user.email}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     port = getenv("API_PORT") or 5000
     host = getenv("API_HOST") or "0.0.0.0"
